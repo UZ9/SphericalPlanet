102c5193529b1083c0204203a0f1308acf677e60
UZ9 <chessboy5@icloud.com>
2020-01-20 19:35:06 -0600

    Initial Commit

---
+++ Assets/Scripts/Noise/Noise.cs
@@ -0,0 +1,25 @@
+using System;
+using UnityEngine;
+
+
+//https://github.com/WardBenjamin/SimplexNoise/blob/master/SimplexNoise/Noise.cs
+public static class Noise
+{
+    public static float Generate(float x, float y, float z)
+    {
+
+        float xy = Mathf.PerlinNoise(x, y);
+        float xz = Mathf.PerlinNoise(x, z);
+        float yz = Mathf.PerlinNoise(y, z);
+        float yx = Mathf.PerlinNoise(y, x);
+        float zx = Mathf.PerlinNoise(z, x);
+        float zy = Mathf.PerlinNoise(z, y);
+
+        return (xy + xz + yz + yx + zx + zy) / 6;
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/WorldGenerator.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class WorldGenerator : MonoBehaviour
+{
+    public int radius;
+
+    public Material material;
+
+    private List<GameObject> cubes = new List<GameObject>();
+
+
+    void Start()
+    {
+        GenerateCubes();
+    }
+
+
+    private void GenerateCubes()
+    {
+        foreach(GameObject cube in cubes) 
+        {
+            StartCoroutine(Destroy(cube));
+        }
+
+        List<List<CombineInstance>> instances = new List<List<CombineInstance>>() {new List<CombineInstance>()};
+
+        int count = 0;
+
+        cubes.Clear();
+
+        Vector3 center = Vector3.one * radius / 2;
+        Debug.Log(center);
+
+        for (int x = 0; x < radius; x++)
+        {
+            for (int y = 0; y < radius; y++)
+            {
+                for (int z = 0; z < radius; z++)
+                {
+                    float noiseValue = Noise.Generate(x * .1f,y * .1f, z * .1f);
+
+                    if (noiseValue >= .5 && (center - new Vector3(x, y, z)).sqrMagnitude < radius / 2 * radius / 2)
+                    {
+                        GameObject cube = (GameObject)Resources.Load("Prefabs/Cube", typeof(GameObject));
+                        cube.transform.position = new Vector3(x, y, z);
+                        if (count > 65533) 
+                        {
+                            instances.Add(new List<CombineInstance>());
+                            count = 0;
+                        }
+
+                        CombineInstance instance = new CombineInstance();
+                        instance.mesh = cube.GetComponent<MeshFilter>().sharedMesh;
+                        instance.transform = cube.transform.localToWorldMatrix;
+                        instances.Last().Add(instance);
+                        //cubes.Add(cube);
+                        count += 24;
+                    
+                    }
+
+
+
+
+                }
+            }
+        }
+
+        int totalMeshCount = 0;
+
+        foreach (List<CombineInstance> list in instances)
+        {
+            totalMeshCount++;
+            GameObject obj = new GameObject("Procedural Mesh " + totalMeshCount);
+            MeshFilter filter = obj.AddComponent<MeshFilter>();
+            MeshRenderer renderer = obj.AddComponent<MeshRenderer>();
+            renderer.material = material;
+            filter.mesh.CombineMeshes(list.ToArray());
+            cubes.Add(obj);
+        }
+
+        
+    }
+
+    
+
+    IEnumerator Destroy(GameObject obj) 
+    {
+        yield return new WaitForEndOfFrame();
+        DestroyImmediate(obj);
+    }
+
+}
